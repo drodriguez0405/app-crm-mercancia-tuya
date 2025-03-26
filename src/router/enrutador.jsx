65e64edb9bd11dbf47f93407812e63725fac0aff
drodriguez0405 <douglaspolo05@gmail.com>
2025-03-26 16:17:56 -0500

    clase nueva

--- src/router/enrutador.jsx
+++ src/router/enrutador.jsx
@@ -1,13 +1,14 @@
 /* El enrutador es un arreglo de objetos */
 /* El objeto se construye con el componente y la ruta */
 import Home from "../pages/Home";
+import Login from "../pages/Login"
 export let enrutador = [
   {
     path: '/home',
     element: <Home />
   },
   {
-    path: '/',
-    element: <h1>Este es el Login</h1>
+    path: '/Login',
+    element: <Login/>
   }
 ]
